90868179c4fd7359504b8501e410fcf34bcc2dc5
Xalanth0204 <88683895+Xalanth0204@users.noreply.github.com>
2026-05-06 15:33:49 +0100

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,18 @@
+export function save(data) {
+  localStorage.setItem("training_state", JSON.stringify(data));
+}
 
+export function load() {
+  return JSON.parse(localStorage.getItem("training_state")) || null;
+}
+
+export function saveHighScore(score) {
+  const current = getHighScore();
+  if (score > current) {
+    localStorage.setItem("high_score", score);
+  }
+}
+
+export function getHighScore() {
+  return parseInt(localStorage.getItem("high_score") || "0");
+}
